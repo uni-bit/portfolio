9496be3f5cedc6d32396733a48aa19af1d6def64
uni-bit <uni11260609@yahoo.co.jp>
2026-02-17 19:24:04 +0900

    Add 404.html for React Router support on GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// GitHub Pages 404.html redirect handling
+if (window.location.pathname.includes('?p=')) {
+  const pathname = window.location.search
+    .split('?p=')[1]
+    .split('&q=')[0]
+    .replace(/~and~/g, '&');
+  const search = window.location.search
+    .split('&q=')[1]
+    ?.replace(/~and~/g, '&') || '';
+  window.history.replaceState(null, null, '/' + pathname + (search ? '?' + search : ''));
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
