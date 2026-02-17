32b4f7faa32b77e626bce79e0617ff903782edac
uni-bit <uni11260609@yahoo.co.jp>
2026-02-17 18:24:43 +0900

    Configure GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter
+      basename={process.env.PUBLIC_URL}
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
